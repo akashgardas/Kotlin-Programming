60accc9837b1a034ad7c47499b17f24c1237abbc
Gardas Akash <akash39g@gmail.com>
2025-09-14 00:33:58 +0530

    Interfaces & Abstract classes

--- src/AquariumFish.kt
+++ src/AquariumFish.kt
@@ -1,17 +1,27 @@
-abstract class AquariumFish: FishAction {
-    abstract val color: String
-    override fun eat() = println("yum")
+interface FishColor {
+    val color: String
 }
 
-class Shark: AquariumFish(), FishAction {
+//abstract class AquariumFish: FishAction {
+//    abstract val color: String
+//    override fun eat() = println("yum")
+//}
+
+// interface delegation
+object GoldColor: FishColor {
+    override val color = "gold"
+}
+
+class Shark: FishColor, FishAction {
     override val color = "grey"
     override fun eat() {
         println("Hunt and eat fish")
     }
 }
 
-class Plecostomus: AquariumFish(), FishAction {
-    override val color = "gold"
+// interface delegation
+class Plecostomus: FishColor by GoldColor, FishAction{
+//    override val color = "gold"
     override fun eat() {
         println("Eat Algae")
     }
