68a0454415484bbc356a9a7ff34325b8e39bb7c8
Gardas Akash <akash39g@gmail.com>
2025-09-14 01:45:03 +0530

    companion objects

--- src/decor/Decoration.kt
+++ src/decor/Decoration.kt
@@ -3,18 +3,29 @@ package decor
 data class Decoration(val rocks: String) {
 }
 
+class Choice {
+    companion object {
+        var name: String = "lyric"
+        fun showDescription(name: String) = println("My favorite $name")
+    }
+}
+
 fun makeDecorations() {
-    val decoration1 = Decoration("granite")
-    println(decoration1)
+    println(Choice.name)
+    Choice.showDescription("pick")
+    Choice.showDescription("selection")
 
-    val decoration2 = Decoration("slate")
-    println(decoration2)
-
-    val decoration3 = Decoration("slate")
-    println(decoration3)
-
-    println(decoration1.equals(decoration2))
-    println(decoration3.equals(decoration2))
+//    val decoration1 = Decoration("granite")
+//    println(decoration1)
+//
+//    val decoration2 = Decoration("slate")
+//    println(decoration2)
+//
+//    val decoration3 = Decoration("slate")
+//    println(decoration3)
+//
+//    println(decoration1.equals(decoration2))
+//    println(decoration3.equals(decoration2))
 }
 
 fun main() {
