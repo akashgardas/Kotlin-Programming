dcb7942b16e1b84f64f2fe99089fcc7503aa7093
Gardas Akash <akash39g@gmail.com>
2025-09-17 22:41:39 +0530

    Foldable Phone

---
+++ src/excercises/FoldablePhone.kt
@@ -0,0 +1,23 @@
+package excercises
+
+open class Phone(var isScreenLightOn: Boolean = false){
+    open fun switchOn() {
+        isScreenLightOn = true
+    }
+
+    fun switchOff() {
+        isScreenLightOn = false
+    }
+
+    fun checkPhoneScreenLight() {
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+        println("The phone screen's light is $phoneScreenLight.")
+    }
+}
+
+class FoldablePhone(isScreenLightOn: Boolean = false, var isFolded: Boolean = true): Phone(isScreenLightOn) {
+    override fun switchOn() {
+        if (isFolded)
+            isScreenLightOn = true
+    }
+}
